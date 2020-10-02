c659a0df763236a3d9e8c201c24205d30eb163be
Nagatsuki46 <nagatsuki46@gmail.com>
2020-10-02 16:22:10 +0900

    HerokuPostgre導入2

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 $dbh = new PDO(
-    'pgsql:host=localhost port=5432 dbname=pgadmin',
-    'postgres',
-    'postgres',
+    'pgsql:host=ec2-52-20-160-44.compute-1.amazonaws.com port=5432 dbname=pgadmin',
+    'ktygjaizcczjhp',
+    'ce53f437af57ef71e88ff29de1a0512f6a3a6604198e3eabae9536bcf819818a',
     [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
 );
 ?>
