ce1bc014fa5c6c39ef61cd3539eabea4a6145599
Nagatsuki46 <nagatsuki46@gmail.com>
2020-10-06 00:11:59 +0900

    レイアウト変更

--- index.php
+++ index.php
@@ -38,8 +38,8 @@ $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
             <td><?php echo htmlspecialchars($r['id']); ?>
             <td style="font-weight:bold;color:#207275;"><?php echo htmlspecialchars($r['title']); ?><br><img src= <?php echo htmlspecialchars($r['thumbnail_url']); ?>>
             <td style="font-size:0.8em"><?php echo htmlspecialchars($r['description']); ?>
-            <td style="font-size:0.8em"><?php echo htmlspecialchars($r['author']); ?><br><td style="font-size:0.8em"><?php echo htmlspecialchars($r['publisher']); ?>
-            <td style="font-size:0.8em"><?php echo htmlspecialchars($r['publishe_date']); ?><br><td style="font-size:0.8em"><?php echo htmlspecialchars($r['entry_date']); ?>
+            <td style="font-size:0.8em"><?php echo htmlspecialchars($r['author']); ?><br><?php echo htmlspecialchars($r['publisher']); ?>
+            <td style="font-size:0.8em"><?php echo htmlspecialchars($r['publishe_date']); ?><br><?php echo htmlspecialchars($r['entry_date']); ?>
             <td style="font-size:0.8em"><?php echo htmlspecialchars($r['isbn']); ?>
     <?php   endforeach; ?>
     </table>
