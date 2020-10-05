10c292d45694fe48f27c84631d82cef062b13e08
ka-fukai <ka-fukai@MBP-Silver.local>
2020-10-05 18:02:03 +0900

    罫線を透明に

--- index.php
+++ index.php
@@ -14,35 +14,41 @@ $sth = $dbh->query(
 $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
-<title>図書の一覧</title>
-<link rel="stylesheet" href="list.css">
-<?php if (!$rows): ?>
-<div>アイテムが見つかりませんでした</div>
-<?php else: ?>
-<table class="Slist">
-    <thead>
+<head>
+    <title>図書の一覧</title>
+    <link rel="stylesheet" href="list.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <?php if (!$rows): ?>
+    <div>アイテムが見つかりませんでした</div>
+    <?php else: ?>
+    <table class="Slist">
+        <thead>
+            <tr>
+                <th scope="col">ID
+                <th scope="col">タイトル
+                <th scope="col">ISBNコード
+                <th scope="col">著者
+                <th scope="col">出版社
+                <th scope="col">出版日
+                <th scope="col">説明
+                <th scope="col">登録日
+                <th scope="col">表紙イメージ
+        <tbody>
+    <?php   foreach($rows as $r): ?>
         <tr>
-            <th scope="col">ID
-            <th scope="col">タイトル
-            <th scope="col">ISBNコード
-            <th scope="col">著者
-            <th scope="col">出版社
-            <th scope="col">出版日
-            <th scope="col">説明
-            <th scope="col">登録日
-            <th scope="col">表紙イメージ
-    <tbody>
-<?php   foreach($rows as $r): ?>
-    <tr>
-        <td><?php echo htmlspecialchars($r['id']); ?>
-        <td><?php echo htmlspecialchars($r['title']); ?>
-        <td><?php echo htmlspecialchars($r['isbn']); ?>
-        <td><?php echo htmlspecialchars($r['author']); ?>
-        <td><?php echo htmlspecialchars($r['publisher']); ?>
-        <td><?php echo htmlspecialchars($r['publishe_date']); ?>
-        <td><?php echo htmlspecialchars($r['description']); ?>
-        <td><?php echo htmlspecialchars($r['entry_date']); ?>
-        <td><img src= <?php echo htmlspecialchars($r['thumbnail_url']); ?>>
-<?php   endforeach; ?>
-</table>
+            <td><?php echo htmlspecialchars($r['id']); ?>
+            <td><?php echo htmlspecialchars($r['title']); ?>
+            <td><?php echo htmlspecialchars($r['isbn']); ?>
+            <td><?php echo htmlspecialchars($r['author']); ?>
+            <td><?php echo htmlspecialchars($r['publisher']); ?>
+            <td><?php echo htmlspecialchars($r['publishe_date']); ?>
+            <td><?php echo htmlspecialchars($r['description']); ?>
+            <td><?php echo htmlspecialchars($r['entry_date']); ?>
+            <td><img src= <?php echo htmlspecialchars($r['thumbnail_url']); ?>>
+    <?php   endforeach; ?>
+    </table>
+</body>
 <?php endif; ?>
