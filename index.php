9a37eeab4c2049064d016b68b47f9f0aac4c33e0
Nagatsuki46 <nagatsuki46@gmail.com>
2020-10-05 23:07:48 +0900

    フォントサイズ修正

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
     <?php   foreach($rows as $r): ?>
         <tr>
             <td><?php echo htmlspecialchars($r['id']); ?>
-            <td style="font-weight:bold;font-size:1.0em;font-color:black;"><?php echo htmlspecialchars($r['title']); ?><br><img src= <?php echo htmlspecialchars($r['thumbnail_url']); ?>>
+            <td style="font-weight:bold;font-size:1.0em;color:black;"><?php echo htmlspecialchars($r['title']); ?><br><img src= <?php echo htmlspecialchars($r['thumbnail_url']); ?>>
             <td><?php echo htmlspecialchars($r['description']); ?>
             <td><?php echo htmlspecialchars($r['author']); ?>
             <td><?php echo htmlspecialchars($r['publisher']); ?>
