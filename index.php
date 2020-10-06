ef16936aa94647d8b31d7a22234169afe497d225
ka-fukai <ka-fukai@MBP-Silver.local>
2020-10-07 00:02:52 +0900

    接続をいったん戻す

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $dsn = sprintf('pgsql:host=%s;dbname=%s',$url['host'],ltrim($url['path'],'/'));
 $pdo = new PDO(
         'pgsql:host=ec2-52-20-160-44.compute-1.amazonaws.com port=5432 dbname=ddncq809usnsnn',
         'ktygjaizcczjhp',
-        $url['pass'],
+        'ce53f437af57ef71e88ff29de1a0512f6a3a6604198e3eabae9536bcf819818a',
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
 );
 
