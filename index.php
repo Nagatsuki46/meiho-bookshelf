8c4b70008d9c8c9995a32afb462143efac2879a5
ka-fukai <ka-fukai@MBP-Silver.local>
2020-10-06 23:29:21 +0900

    PDO接続を環境変数に対応

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
 <?php
-$dbh = new PDO(
 
-    /*
+$url = parse_url(getenv('DATABASE_URL'));
+$dsn = sprintf('pgsql:host=%s;dbname=%s',$url['host'],substr($url['path'],1));
+$pdo = new PDO($dsn,$url['user'],$url['pass']);
+
+/*
+$dbh = new PDO(
     //ローカルDB接続用
     'pgsql:host=localhost port=5432 dbname=pgadmin',
     'postgres',
     'postgres',
     [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION] 
-    */
     
     'pgsql:host=ec2-52-20-160-44.compute-1.amazonaws.com port=5432 dbname=ddncq809usnsnn',
     'ktygjaizcczjhp',
     'ce53f437af57ef71e88ff29de1a0512f6a3a6604198e3eabae9536bcf819818a',
     [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-    
 );
-$sth = $dbh->query(
+*/
+
+$sth = $dbo->query(
 //    'SELECT id, title, isbn, author, CASE WHEN publisher IS NOT NULL THEN \'[\' || publisher || \']\' END AS publisher,'
 //    . 'publishe_date, description, \'[\' || entry_date || \']\' AS entry_date, thumbnail_url'
     'SELECT id, title, isbn, author, publisher,'
@@ -39,20 +43,19 @@ $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
         <thead>
             <tr>
                 <th scope="col">ID
-                <th scope="col">Title
+                <th scope="col">Title<br>
                 <th scope="col">Description
                 <th scope="col">Author<br><div class="tr_div">Publisher</div>
                 <th scope="col">Publishe date<br><div class="tr_div">Entry date</div>
-                <th scope="col">ISBN CD
         <tbody>
     <?php   foreach($rows as $r): ?>
         <tr>
             <td><?php echo htmlspecialchars($r['id']); ?>
             <td class="td_title"><?php echo htmlspecialchars($r['title']); ?><br><img src= <?php echo htmlspecialchars($r['thumbnail_url']); ?>>
+                <br><div class="td_isbn">ISBN:<?php echo htmlspecialchars($r['isbn']); ?></div>
             <td class="td_details"><?php echo htmlspecialchars($r['description']); ?>
             <td class="td_details"><?php echo htmlspecialchars($r['author']); ?><br><div class="td_div"><?php echo htmlspecialchars($r['publisher']); ?></div>
             <td class="td_details"><?php echo htmlspecialchars($r['publishe_date']); ?><br><div class="td_div"><?php echo htmlspecialchars($r['entry_date']); ?></div>
-            <td class="td_details"><?php echo htmlspecialchars($r['isbn']); ?>
     <?php   endforeach; ?>
     </table>
 </body>
