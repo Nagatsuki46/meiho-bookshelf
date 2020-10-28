406e9fb33602d69907d5354831a4d0e483ba0d89
ka-fukai <ka-fukai@MBP-Silver.local>
2020-10-28 15:05:44 +0900

    借受者の名前が文字化けしgているのを修正

--- return.php
+++ return.php
@@ -139,8 +139,8 @@
       </dl>
       <hr class="hr01">
       <dl class="edit">
-          <dt class="dt_details">借受者情報（苗字のみ）
-          <dd class="dt_div"><?php echo rawurlencode($origin['employee_id']); ?>
+          <dt class="dt_details">借受者（苗字のみ）
+          <dd class="dt_div"><?php echo htmlspecialchars($origin['employee_id']); ?>
           <dt class="dt_details">貸出日
           <dd class="dt_div"><?php echo rawurlencode($origin['checkout_date']); ?>
           <dt class="dt_details">返却予定日
